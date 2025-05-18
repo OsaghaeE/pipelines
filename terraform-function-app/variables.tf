811170c1280b7449b431985da65d36e4d7a7a37f
OsaghaeE <osaghaeedobor@hotmail.com>
2025-05-19 02:08:19 +1000

    Create variables.tf

---
+++ terraform-function-app/variables.tf
@@ -0,0 +1,15 @@
+variable "resource_group_name" {
+  default = "rg-devops-func"
+}
+
+variable "location" {
+  default = "Australia East"
+}
+
+variable "storage_account_name" {
+  default = "funcappstoragedev"
+}
+
+variable "function_app_name" {
+  default = "devops-function-app"
+}
