b38908b74a15062b2ac876b13fcf1ecba599304f
OsaghaeE <osaghaeedobor@hotmail.com>
2025-05-19 02:07:13 +1000

    Create main.tf

---
+++ terraform-function-app/main.tf
@@ -0,0 +1,52 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "function_rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_storage_account" "function_storage" {
+  name                     = var.storage_account_name
+  resource_group_name      = azurerm_resource_group.function_rg.name
+  location                 = var.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_app_service_plan" "function_plan" {
+  name                = "${var.function_app_name}-plan"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.function_rg.name
+  kind                = "FunctionApp"
+  reserved            = true
+
+  sku {
+    tier = "Dynamic"
+    size = "Y1"
+  }
+}
+
+resource "azurerm_function_app" "function" {
+  name                       = var.function_app_name
+  location                   = var.location
+  resource_group_name        = azurerm_resource_group.function_rg.name
+  app_service_plan_id        = azurerm_app_service_plan.function_plan.id
+  storage_account_name       = azurerm_storage_account.function_storage.name
+  storage_account_access_key = azurerm_storage_account.function_storage.primary_access_key
+  os_type                    = "linux"
+  version                    = "~4"
+  functions_extension_version = "~4"
+
+  site_config {
+    application_stack {
+      python_version = "3.10"
+    }
+  }
+
+  app_settings = {
+    FUNCTIONS_WORKER_RUNTIME = "python"
+    WEBSITE_RUN_FROM_PACKAGE = "1"
+  }
+}
